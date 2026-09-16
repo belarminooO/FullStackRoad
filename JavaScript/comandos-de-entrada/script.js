e47963aa457e0f5a470f83d837069f577975eb5d
belarminooO <130702154+belarminooO@users.noreply.github.com>
2026-09-16 13:57:22 +0100

    JS - comandos de entrada

---
+++ JavaScript/comandos-de-entrada/script.js
@@ -0,0 +1,11 @@
+// alert(`teste`);
+
+var nome = prompt("Digite o seu nome");
+
+var apelido = prompt("Digite seu apelido");
+
+console.log("Ola " + nome + " " + apelido);
+
+document.write("<h1>Bem vindo " + nome + "</h1>")
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do steve' />");
