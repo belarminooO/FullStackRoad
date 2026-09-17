77a7610e4973191a3a124ca7ef733d2ae6403d1b
belarminooO <130702154+belarminooO@users.noreply.github.com>
2026-09-17 13:13:40 +0100

    JS  - condicionais e comparacoes pt-2

--- JavaScript/condicionais-e-comparacoes/script.js
+++ JavaScript/condicionais-e-comparacoes/script.js
@@ -1,3 +1,4 @@
+//parte 1
 var valor = 30;
 
 // == compara so os valores e nao olha para o tipo.
@@ -8,4 +9,33 @@ if(valor === 30){
   console.log('sim o valor E igual a 30');
 }else{
   console.log('numero diferente de 30!');
+}
+
+// parte 2
+// tambem podemos comparar strings e booleanos.
+
+var nome = "Belarmino";
+var userOnline = true;
+var numero = 10;
+
+//comparacao de strings
+if(nome === 'Belarmino'){
+  console.log('Bem vindo Belarmino');
+}
+
+//comparacao booleana
+if(userOnline === true){
+  console.log('usuario esta online!');
+}else{
+  console.log('usuario offline!');
+}
+
+// usando operador ternario para simplificar a escrita do 'if else'
+numero === 10 ? console.log('o numero e igual a 10') : console.log('o numero nao e igual a 10')
+
+// e o mesmo que:
+if(numero === 10){
+  console.log('o numero igual a 10');
+}else{
+  console.log('o numero nao e igual a 10');
 }
