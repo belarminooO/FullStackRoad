5170f0e2b46b9af5748f5d1149763d8e06f717b2
belarminooO <130702154+belarminooO@users.noreply.github.com>
2026-09-17 12:59:54 +0100

    JS  - condicionais e comparacoes

---
+++ JavaScript/condicionais-e-comparacoes/script.js
@@ -0,0 +1,11 @@
+var valor = 30;
+
+// == compara so os valores e nao olha para o tipo.
+// === compara o valor e o tipo
+// outros tipos de comparacoes: <, >, <=, >=.
+
+if(valor === 30){
+  console.log('sim o valor E igual a 30');
+}else{
+  console.log('numero diferente de 30!');
+}
