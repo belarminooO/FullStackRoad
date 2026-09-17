286b1fbe3003211012fd37ad2f5537a2599e5a6d
belarminooO <130702154+belarminooO@users.noreply.github.com>
2026-09-17 12:29:39 +0100

    JS - loops

---
+++ JavaScript/loops/loops.js
@@ -0,0 +1,17 @@
+//while loop
+var x = 0;
+
+while(x < 10){ //enquanto x for...
+  console.log('o valor de x: ', x)
+  x++;
+}
+
+console.log('----------------');
+
+// for loop
+var valor = 15;
+for(a = 0; a <= valor; a++){
+  console.log('valor de x: ', a);
+}
+
+
