35c88ff5a603c4a952d4161205fcdcbf4d9ca6ea
belarminooO <130702154+belarminooO@users.noreply.github.com>
2026-09-21 14:32:44 +0100

    PRJ - lista de tarefas: listando as tarefas na pagina

--- Projetos/Lista de tarefas/script.js
+++ Projetos/Lista de tarefas/script.js
@@ -4,6 +4,20 @@ let buttonElement = document.querySelector('#app button'); // pegando o botao...
 
 let tarefas = [];
 
+function renderTarefas(){
+  listElement.innerHTML = '';
+
+  // percorendo os lementos com o map, usando funcao anonima para trabalhar
+  // sobre cada elemento, passando para a mesma cada elemento (todo).
+  tarefas.map((todo) => {
+    let liElement = document.createElement('li'); //criando um li
+    let tarefaText = document.createTextNode(todo); // criando o texto.
+
+    liElement.appendChild(tarefaText); //colocando o texto na li
+    listElement.appendChild(liElement); // colocando oento li na lista de tarefas 'ul'
+  })
+}
+
 function adicionarTarefas(){
   if(inputElement.value === ''){
     alert('digite alguma tarefa');
@@ -11,7 +25,9 @@ function adicionarTarefas(){
   }else{
     let novaTarefa = inputElement.value; // pegando a tarefa digita
     tarefas.push(novaTarefa); // adicionando a lista de tareas
-    inputElement.value = '';
+    inputElement.value = ''; // resetando o input.
+    renderTarefas(); // chamando a funcao para a renderizacao de tarefas registadas.
   }
 }
+
 
